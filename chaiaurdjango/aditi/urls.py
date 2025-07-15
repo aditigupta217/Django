81592570da0733e6a2c4be6a562987f46fd0e088
aditigupta217 <aditigupta221177@gmail.com>
2025-07-15 22:52:33 +0530

    django

--- chaiaurdjango/aditi/urls.py
+++ chaiaurdjango/aditi/urls.py
@@ -7,7 +7,6 @@ from . import views
 #localhost:8000/aditi/order
 urlpatterns = [
    
-    path('',views.all_chai , name='all_home'),
-    path( 'order/', views.order, name='order'), 
-   
+    path('',views.all_chai , name='all_chai'),
+
 ]
