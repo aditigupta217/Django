f60d541785b34c5bcab0dd44d3d3f65a9716666f
aditigupta217 <aditigupta221177@gmail.com>
2025-09-29 22:29:14 +0530

    add

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("This is home page")
+   # return HttpResponse("This is home page")
+   return render(request,'index.html')
 
 def about(request):
     return HttpResponse("This is about page")
